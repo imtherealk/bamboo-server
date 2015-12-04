f0d8aa029a469a024080a7e158a6ce9c013e9a05
imtherealk <imtherealk@gmail.com>
2015-12-04 19:27:08 +0900

    admin에 모델 등록

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api.models import Bamboo, Comment, Post, Report, User, BambooAdmin
 
-# Register your models here.
+admin.site.register(Bamboo)
+admin.site.register(Comment)
+admin.site.register(Post)
+admin.site.register(Report)
+admin.site.register(User)
+admin.site.register(BambooAdmin)
