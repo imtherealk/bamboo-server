0b9d7e22c4635c51f5fdf21dda0ad342a730ebcd
imtherealk <imtherealk@gmail.com>
2015-12-07 19:11:38 +0900

    기본적인 권한 설정

--- api/views/user.py
+++ api/views/user.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers, viewsets
 from rest_framework.decorators import detail_route
+from rest_framework.permissions import AllowAny, BasePermission
 
 from api.models import User
 
@@ -10,6 +11,23 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'url', 'username', 'email', 'is_staff')
 
 
+class UserPermission(BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            return True
+        if request.user.is_superuser:
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        if obj == request.user:
+            return True
+        return False
+
+
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (UserPermission,)
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
