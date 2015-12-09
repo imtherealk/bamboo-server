b7569f7c54baf74aad77477e75ff3fdaff484d66
imtherealk <imtherealk@gmail.com>
2015-12-09 19:13:49 +0900

    API url 추가

--- bamboo/urls.py
+++ bamboo/urls.py
@@ -3,23 +3,50 @@ from django.contrib import admin
 from rest_framework import routers
 from api.views.bamboo import BambooViewSet
 from api.views.comment import CommentViewSet
+from api.views.manager import BambooAdminViewSet
 from api.views.post import PostViewSet
 from api.views.report import ReportViewSet
 from api.views.user import UserViewSet
 
 router = routers.DefaultRouter()
 
-# Register API URLs
+# Register API URLs\
 router.register(r'user', UserViewSet)
 router.register(r'bamboo', BambooViewSet)
-router.register(r'bamboo/(?P<bamboo_pk>.+)/manager', BambooViewSet)
-router.register(r'comment', CommentViewSet)
-router.register(r'post', PostViewSet)
-router.register(r'report', ReportViewSet)
+router.register(r'bamboo/(?P<bamboo_pk>.+)/manager', BambooAdminViewSet)
 
 
 urlpatterns = patterns(
     '',
+    # Post CRUD URLs
+    url(r'^report/(?P<report_pk>.+)/post/', PostViewSet.as_view({
+        'post': 'create',
+    })),
+    url(r'^bamboo/(?P<bamboo_pk>.+)/post/', PostViewSet.as_view({
+        'get': 'list',
+    })),
+    url(r'^post/(?P<post_pk>.+)/', PostViewSet.as_view({
+        'get': 'retrieve',
+        'delete': 'destroy',
+    })),
+    # Report CRUD URLs
+    url(r'bamboo/(?P<bamboo_pk>.+)/report/', ReportViewSet.as_view({
+        'post': 'create',
+        'get': 'list',
+    })),
+    url(r'report/(?P<report_pk>.+)', ReportViewSet.as_view({
+        'get': 'retrieve',
+        'delete': 'destroy',
+    })),
+    # Comment CRUD URLs
+    url(r'^post/(?P<post_pk>.+)/comment/', CommentViewSet.as_view({
+        'post': 'create',
+        'get': 'list',
+    })),
+    url(r'comment/(?P<pk>.+)', CommentViewSet.as_view({
+        'get': 'retrieve',
+        'delete': 'destroy',
+    })),
     url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
