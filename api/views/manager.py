67bd006915b86d2069fdb2bf825764ff77fac110
imtherealk <imtherealk@gmail.com>
2015-12-09 19:17:21 +0900

    BambooAdmin에서 존재하지 않는 Bamboo에 대해 404에러 발생 get_queryset을 수정하여, bamboo의 존재 여부를 체크함

--- api/views/manager.py
+++ api/views/manager.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers, viewsets
 
-from api.models import BambooManager
+from api.models import BambooManager, Bamboo
 
 
 class BambooAdminSerializer(serializers.HyperlinkedModelSerializer):
@@ -15,5 +16,6 @@ class BambooAdminViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         bamboo_pk = self.kwargs['bamboo_pk']
-        queryset = self.queryset.filter(bamboo__id=bamboo_pk)
+        bamboo = get_object_or_404(Bamboo.objects.filter(id=bamboo_pk))
+        queryset = self.queryset.filter(bamboo__pk=bamboo.id)
         return queryset
