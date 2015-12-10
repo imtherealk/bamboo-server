44cbc77956ff1e7ad554e2e63768178bb6d42b78
imtherealk <imtherealk@gmail.com>
2015-12-10 20:14:29 +0900

    제보 create API작성

--- api/views/report.py
+++ api/views/report.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers, viewsets
-from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 from api.models import Report, Bamboo
 
@@ -8,11 +9,15 @@ from api.models import Report, Bamboo
 class ReportSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Report
-        fields = ('id', 'content', 'message', 'bamboo')
+        fields = ('id', 'content', 'message', 'bamboo', 'created_at')
 
 
-class ReportViewSet(viewsets.mixins.CreateModelMixin,
-                    viewsets.mixins.RetrieveModelMixin,
+class ReportCreateForm(forms.Form):
+    content = forms.Field()
+    message = forms.Field(required=False)
+
+
+class ReportViewSet(viewsets.mixins.RetrieveModelMixin,
                     viewsets.mixins.DestroyModelMixin,
                     viewsets.mixins.ListModelMixin,
                     viewsets.GenericViewSet):
@@ -26,3 +31,20 @@ class ReportViewSet(viewsets.mixins.CreateModelMixin,
             bamboo = get_object_or_404(Bamboo.objects.filter(id=bamboo_pk))
             queryset = queryset.filter(bamboo=bamboo)
         return queryset
+
+    def create(self, request, bamboo_pk):
+        bamboo = get_object_or_404(Bamboo.objects.filter(id=bamboo_pk))
+        form = ReportCreateForm(request.data)
+        if form.is_valid():
+            content = form.cleaned_data['content']
+            message = form.cleaned_data['message']
+            report = Report.objects.create(
+                content=content,
+                message=message,
+                bamboo=bamboo,
+                writer=request.user,
+            )
+            report.save()
+            serializer = self.get_serializer(report)
+            return Response(serializer.data, 201)
+        return Response(form.errors, 400)
