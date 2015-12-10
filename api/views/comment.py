1e8b94a5e113cead27a5b6059059998e885f482d
imtherealk <imtherealk@gmail.com>
2015-12-10 23:25:36 +0900

    comment create API 작성

--- api/views/comment.py
+++ api/views/comment.py
@@ -1,6 +1,8 @@
+from django.forms import forms
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers, viewsets
 from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 from api.models import Comment, Post
 
@@ -11,8 +13,11 @@ class CommentSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'content', 'created_at', 'post')
 
 
-class CommentViewSet(viewsets.mixins.CreateModelMixin,
-                     viewsets.mixins.RetrieveModelMixin,
+class CommentCreateForm(forms.Form):
+    content = forms.Field()
+
+
+class CommentViewSet(viewsets.mixins.RetrieveModelMixin,
                      viewsets.mixins.DestroyModelMixin,
                      viewsets.mixins.ListModelMixin,
                      viewsets.GenericViewSet):
@@ -27,3 +32,17 @@ class CommentViewSet(viewsets.mixins.CreateModelMixin,
             queryset = queryset.filter(post=post)
         return queryset
 
+    def create(self, request, post_pk):
+        post = get_object_or_404(Post.objects.filter(id=post_pk))
+        form = CommentCreateForm(request.data)
+        if form.is_valid():
+            content = form.cleaned_data['content']
+            comment = Comment.objects.create(
+                content=content,
+                post=post,
+                writer=request.user,
+            )
+            comment.save()
+            serializer = self.get_serializer(comment)
+            return Response(serializer.data, 201)
+        return Response(form.errors, 400)
