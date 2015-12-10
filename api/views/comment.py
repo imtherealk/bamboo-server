15088e27dce790c745a2c76f4452798e96534827
imtherealk <imtherealk@gmail.com>
2015-12-10 18:49:42 +0900

    queryset에 필터 적용

--- api/views/comment.py
+++ api/views/comment.py
@@ -1,7 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers, viewsets
 from rest_framework.decorators import detail_route
 
-from api.models import Comment
+from api.models import Comment, Post
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,3 +18,12 @@ class CommentViewSet(viewsets.mixins.CreateModelMixin,
                      viewsets.GenericViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if 'post_pk' in self.kwargs:
+            post_pk = self.kwargs['post_pk']
+            post = get_object_or_404(Post.objects.filter(id=post_pk))
+            queryset = queryset.filter(post=post)
+        return queryset
+
--- api/views/report.py
+++ api/views/report.py
@@ -1,13 +1,14 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers, viewsets
 from rest_framework.decorators import detail_route
 
-from api.models import Report
+from api.models import Report, Bamboo
 
 
 class ReportSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Report
-        fields = ('id', 'content', 'message', 'created_at', 'bamboo')
+        fields = ('id', 'content', 'message', 'bamboo')
 
 
 class ReportViewSet(viewsets.mixins.CreateModelMixin,
@@ -17,3 +18,11 @@ class ReportViewSet(viewsets.mixins.CreateModelMixin,
                     viewsets.GenericViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if 'bamboo_pk' in self.kwargs:
+            bamboo_pk = self.kwargs['bamboo_pk']
+            bamboo = get_object_or_404(Bamboo.objects.filter(id=bamboo_pk))
+            queryset = queryset.filter(bamboo=bamboo)
+        return queryset
